dbd504846ac1a7100cc2ae9f01ff594a7aac9782
SynthesisOne <34944650+SynthesisOne@users.noreply.github.com>
2019-08-10 15:47:49 +0300

    'views'

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -4,9 +4,16 @@ namespace app\controllers;
 
 class MainController extends AppController {
 
-    public function indexAction(){
-        echo __METHOD__;
+//    public $layout = 'test';
 
+    public function indexAction(){
+//        $this->layout = 'test';
+//        echo __METHOD__;
+        $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+        $name = 'John';
+        $age = 30;
+
+        $this->set(['name' => 'John','age'=>'30']);
     }
 
 }
