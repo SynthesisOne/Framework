f0107451b9ca0b9baf24a3711427c23a8ae73885
SynthesisOne <34944650+SynthesisOne@users.noreply.github.com>
2019-08-13 20:30:58 +0300

    'redbean'

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -2,18 +2,26 @@
 
 namespace app\controllers;
 
+use ishop\Cache;
+
 class MainController extends AppController {
 
-//    public $layout = 'test';
-
     public function indexAction(){
-//        $this->layout = 'test';
-//        echo __METHOD__;
+        $posts = \R::findAll('test');
+        $post = \R::findOne('test', 'id = ?', [2]);
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
         $name = 'John';
         $age = 30;
-        $names = ['Andrey', 'Jane',];
-        $this->set(compact('name', 'age', 'names'));
+        $names = ['Andrey', 'Jane', 'Mike'];
+        $cache = Cache::instance();
+        //$cache->set('test', $names);
+//        $cache->delete('test');
+        $data = $cache->get('test');
+        if(!$data){
+            $cache->set('test', $names);
+        }
+        debug($data);
+        $this->set(compact('name', 'age', 'names', 'posts'));
     }
 
 }
