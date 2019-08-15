eabbc6a03ae20de792b8bfd6998185a3a8ae7fd1
SynthesisOne <34944650+SynthesisOne@users.noreply.github.com>
2019-08-15 15:40:40 +0300

    'currency_change'

--- app/widgets/currency/Currency.php
+++ app/widgets/currency/Currency.php
@@ -2,6 +2,8 @@
 
 namespace app\widgets\currency;
 
+use ishop\App;
+
 class Currency{
 
     protected $tpl;
@@ -14,8 +16,9 @@ class Currency{
     }
 
     protected function run(){
-
-        $this->getHtml();
+        $this->currencies = App::$app->getProperty('currencies');
+        $this->currency = App::$app->getProperty('currency');
+        echo $this->getHtml();
     }
 
     public static function getCurrencies(){
@@ -25,22 +28,18 @@ class Currency{
     public static function getCurrency($currencies){
         if(isset($_COOKIE['currency']) && array_key_exists($_COOKIE['currency'], $currencies)){
             $key = $_COOKIE['currency'];
-        }else
-            {
+        }else{
             $key = key($currencies);
-
-
-           }
-
+        }
         $currency = $currencies[$key];
-
-
         $currency['code'] = $key;
         return $currency;
     }
 
     protected function getHtml(){
-
+        ob_start();
+        require_once $this->tpl;
+        return ob_get_clean();
     }
 
 }
--- vendor/ishop/core/App.php
+++ vendor/ishop/core/App.php
@@ -8,6 +8,7 @@ class App{
 
     public function __construct(){
         $query = trim($_SERVER['QUERY_STRING'], '/');
+
         session_start();
         self::$app = Registry::instance();
         $this->getParams();
