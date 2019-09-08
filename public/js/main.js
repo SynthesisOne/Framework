a27f41f5506d1b93e6905ad46b9576ab0e3e4edf
SynthesisOne <34944650+SynthesisOne@users.noreply.github.com>
2019-09-08 23:39:46 +0300

    Cart

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,27 @@
+/*Cart*/
+$('body').on('click', '.add-to-cart-link', function(e){
+     e.preventDefault();
+     var id = $(this).data('id'),
+         qty = $('.quantity input').val() ? $('.quantity input').val() : 1,
+         mod = $('.available select').val();
+     $.ajax({
+         url: '/cart/add',
+         data: {id: id, qty: qty, mod: mod},
+         type: 'GET',
+         success: function(res){
+             showCart(res);
+         },
+         error: function(){
+             alert('Ошибка! Попробуйте позже');
+         }
+     });
+});
+
+function showCart(cart){
+    console.log(cart);
+}
+/*Cart*/
+
 $('#currency').change(function(){
     window.location = 'currency/change?curr=' + $(this).val();
 });
@@ -7,11 +31,9 @@ $('.available select').on('change', function(){
         color = $(this).find('option').filter(':selected').data('title'),
         price = $(this).find('option').filter(':selected').data('price'),
         basePrice = $('#base-price').data('base');
-
     if(price){
         $('#base-price').text(symboleLeft + price + symboleRight);
     }else{
         $('#base-price').text(symboleLeft + basePrice + symboleRight);
     }
-    console.log(modId,color,price);
 });
