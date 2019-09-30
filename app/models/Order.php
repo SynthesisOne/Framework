114555b75c936949a6549b7ede58abaaa4daa944
SynthesisOne <34944650+SynthesisOne@users.noreply.github.com>
2019-10-01 02:12:18 +0300

    sql_in_order_item

--- app/models/Order.php
+++ app/models/Order.php
@@ -4,22 +4,27 @@ namespace app\models;
 
 class Order extends AppModel {
 
-
-
-    public  function saveOrder($data){
-       $this->attributes['user_id'] = $data['user_id'];
-        $this->attributes['currency'] = $_SESSION['cart.currency']['code'];
-        $this->attributes['note'] = $data['note'];
-      $order_id =  $this->save('order');
-
-      self::saveOrderProduct($order_id);
+    public static function saveOrder($data){
+        $order = \R::dispense('order');
+        $order->user_id = $data['user_id'];
+        $order->note = $data['note'];
+        $order->currency = $_SESSION['cart.currency']['code'];
+        $order_id = \R::store($order);
+        self::saveOrderProduct($order_id);
+        return $order_id;
     }
 
     public static function saveOrderProduct($order_id){
-
+        $sql_part = '';
+        foreach($_SESSION['cart'] as $product_id => $product){
+            $product_id = (int)$product_id;
+            $sql_part .= "($order_id, $product_id, {$product['qty']}, '{$product['title']}', {$product['price']}),";
+        }
+        $sql_part = rtrim($sql_part, ',');
+        \R::exec("INSERT INTO order_product (order_id, product_id, qty, title, price) VALUES $sql_part");
     }
 
-    public  function mailOrder($order_id, $user_email){
+    public static function mailOrder($order_id, $user_email){
 
     }
 
